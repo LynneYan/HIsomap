71f0f5b37ff49db77dfa7319fb9991787084dad2
Lin Yan <linyan@lindembp.uconnect.utah.edu>
2019-12-09 15:15:04 -0700

    add instruction

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # HIsomap
+
+## How to use:
+
+Tested with Python 2.7, MacOS and Linux.
+
+## Installation
+   $ sudo python setup.py install
+
+### Run
+   $ python test.py
+
+
--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from distutils.core import setup
 
 setup(name='HIsomap',
 version='1.0',
-py_modules=['HIsomap'],
+py_modules=['HIsomap', 'KernelPCA', 'km', 'progressreporter'],
 )
