5d05f90d2cc21795a2df26bafc6d764219c169d0
Lin Yan <linyan@lindembp.uconnect.utah.edu>
2019-12-12 11:56:43 -0700

    fix test.py for python2&3

--- test.py
+++ test.py
@@ -1,7 +1,12 @@
 from HIsomap import HIsomap
 import numpy as np
+import sys
 
 X = np.loadtxt('X.txt')
-proj = HIsomap(nr_cubes=20, show_skeleton="on", show_projection="on",
-               filter_function="base_point_geodesic_distance")
+if sys.version_info >= (3, 0):
+    proj = HIsomap.HIsomap(nr_cubes=20, show_skeleton="on", show_projection="on",
+                   filter_function="base_point_geodesic_distance")
+else:
+    proj = HIsomap(nr_cubes=20, show_skeleton="on", show_projection="on",
+                   filter_function="base_point_geodesic_distance")
 Y = proj.fit_transform(X)
